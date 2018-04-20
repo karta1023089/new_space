2e33578ecf5d2b83d11815d1523c4769b1e0b176
丁昱翔 <joe22420@gmail.com>
2018-04-20 15:41:41 +0800

    combine tour

--- app/controllers/home/backpackers_controller.rb
+++ app/controllers/home/backpackers_controller.rb
@@ -1,5 +1,8 @@
 class Home::BackpackersController < HomeController
 	def index
+		@the_category = Category.find(params[:category])
+		@combines = @the_category.combine_projects
+		@the_group = @combines.pluck(:the_group).uniq
 	end
 	def show
 		@combine = CombineProject.find(params[:id])
