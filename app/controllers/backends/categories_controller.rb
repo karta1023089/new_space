d5d2c5642e519aa8799c7f9a55aa49793041670e
丁昱翔 <joe22420@gmail.com>
2018-04-23 16:24:21 +0800

    try

--- app/controllers/backends/categories_controller.rb
+++ app/controllers/backends/categories_controller.rb
@@ -2,7 +2,7 @@ class Backends::CategoriesController < BackendsController
   before_action :find_category
   helper_method :current_cart
   def index
-    Category.category_type.create(:parent_id => 2,:name => "團體行程")
+    Category.origin.create(:parent_id => 2,:name => "團體行程")
     if Category.category_types.keys.include?(params[:category_type])
       @categories = Category.send(params[:category_type]).where(parent_id: nil).order_by_priority_asc
     else
